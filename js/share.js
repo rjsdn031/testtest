e70b532c3d86f3e175ff16ee98ea41df18c9fb25
rjsdn031 <rjsdn031@naver.com>
2023-07-19 16:18:23 +0900

    output patch

--- js/share.js
+++ js/share.js
@@ -3,21 +3,19 @@ url = 'https://amuuutest.netlify.app'
 function setShare() {
 	let resultImg = document.querySelector("#resultImg");
 	let resultAlt = resultImg.firstElementChild.alt;
-	const shareTitle = '아무테스트 결과'
-	const shareDesc = infoList[resultAlt].name;
+	// const shareTitle = '아무테스트 결과'
+	// const shareDesc = infoList[resultAlt].name;
 	const shareImage = url + '/img/image-' + resultAlt + '.png';
 	const shareURL = url + '/page/result-' + resultAlt + '.html';
 
-}
-
-function kakaoShare() {
 	Kakao.Share.sendDefault({
 		objectType: 'text',
 		text:
 			'아무튼 테스트를 테스트하고 있는데 무슨 말을 쓸 지 모르겠습니다. 여러분의 테스트 결과를 확인하세요!',
 		link: {
-		  	mobileWebUrl: url,
-		  	webUrl: url,
+				mobileWebUrl: shareURL,
+				webUrl: shareURL,
 		},
 	});
 }
+
